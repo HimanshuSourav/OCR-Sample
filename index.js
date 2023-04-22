587903cac18911415c7f0e8b19370126740a4558
HimanshuSourav <himanshu.sourav63@gmail.com>
2023-04-21 20:55:01 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+//--File-upload working version
+import FileUpload from './FileUpload';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+ReactDOM.render(
+<React.StrictMode>
+ <FileUpload />
+</React.StrictMode>,
+document.getElementById('root')
+ );
+
